9c077865a0558e39c021ae7f5df76fc16a973483
lianghhhhh <selenahuang0218@gmail.com>
2025-11-06 19:00:45 +0800

    normalize data before input to model, denormalize data after output from model, modify warning message

--- src/car_control_pkg/car_control_pkg/car_control_node.py
+++ src/car_control_pkg/car_control_pkg/car_control_node.py
@@ -1,7 +1,8 @@
+import numpy as np
 import casadi as ca
 from rclpy.node import Node
 from std_msgs.msg import Float32MultiArray
-from car_control_pkg.utils import loadModelFunc, createMpcSolver
+from car_control_pkg.utils import loadModelFunc, createMpcSolver, normalize, denormalize
 
 class CarControlNode(Node):
     def __init__(self, car_state_node, path_points_node):
@@ -19,6 +20,7 @@ class CarControlNode(Node):
         self.create_timer(0.01, lambda: self.find_control_command(10))
 
     def publish_control_command(self, control_input):
+        self.get_logger().info(f'Publishing control command: {control_input}')
         front_msg = Float32MultiArray()
         rear_msg = Float32MultiArray()
         vals = [float(x) for x in control_input]
@@ -29,10 +31,16 @@ class CarControlNode(Node):
 
     def find_control_command(self, N=10):
         current_state = self.car_state_node.car_state
-        path_points = self.path_points_node.path_points[:(N+1)*2]
-        if len(current_state) != 4 or len(path_points) != (N+1)*2:
-            self.get_logger().warn('Insufficient data for MPC computation.')
+        path_points = self.path_points_node.get_near_points(current_state[0:2], num_points=(N+1)*2)
+        if len(current_state) != 4:
+            self.get_logger().warn(f'Invalid car state received: {current_state}')
             return
+        if len(path_points) != (N+1)*2:
+            self.get_logger().warn(f'Invalid path points received: {path_points}')
+            return
+        
+        current_state = normalize(current_state, "x")
+        path_points = normalize(np.array(path_points).reshape(2, -1), "x") # reshape to (N+1, 2)
         target_path_data = ca.DM(path_points).reshape((N+1, 2))
 
         self._solver.set_value(self._current_x, current_state)
@@ -42,6 +50,7 @@ class CarControlNode(Node):
             solution = self._solver.solve()
             optimal_control = solution.value(self._u_pred)
             control_input = optimal_control[0, :]
+            control_input = denormalize(control_input, "u")
             self.publish_control_command(control_input)
         except Exception as e:
             self.get_logger().error(f'MPC solver failed: {e}')
